03889ae95da66bb184d38455c5c5afd4c73b91fc
BLAXPIAS05 <reaganw762@gmail.com>
2023-08-11 11:29:40 -0600

    initialize snkz project

---
+++ lib/snkz_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule SnkzWeb.PageHTML do
+  use SnkzWeb, :html
+
+  embed_templates "page_html/*"
+end
---
+++ test/snkz_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule SnkzWeb.ErrorHTMLTest do
+  use SnkzWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(SnkzWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(SnkzWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
