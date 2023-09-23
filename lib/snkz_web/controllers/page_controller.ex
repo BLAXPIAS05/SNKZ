dd40ca60055f78af11d28c9840c7a5e7bf2397a9
BLAXPIAS05 <reaganw762@gmail.com>
2023-09-23 18:44:31 -0400

    home page complete

--- lib/snkz_web/controllers/page_controller.ex
+++ lib/snkz_web/controllers/page_controller.ex
@@ -4,6 +4,6 @@ defmodule SnkzWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home, layout: false, background_color: "bg-[#b4d6e3]")
   end
 end
